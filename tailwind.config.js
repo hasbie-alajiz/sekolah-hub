eae7ff097ba2d9d7435e1cc54cf05b48b6d47fec
kuromu <94413730+chrome-sum@users.noreply.github.com>
2026-06-19 14:57:09 +0700

    docs(frontend): add comprehensive frontend architecture documentation

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './app/Modules/**/*.blade.php',
+        './themes/**/*.blade.php',
     ],
 
     theme: {
